5a72b34f4ef07c9868f09efa10d8434531e002bf
csgohh <gohchunsheng.gch10@gmail.com>
2026-06-18 11:58:13 +0800

    Update SupabaseClient.kt

--- app/src/main/java/com/example/ovagrown/database/SupabaseClient.kt
+++ app/src/main/java/com/example/ovagrown/database/SupabaseClient.kt
@@ -1,15 +1,20 @@
-package com.example.ovagrown.database
+package com.example.overgrown.database
 
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.postgrest.Postgrest
 
 object SupabaseClient {
+
     val client = createSupabaseClient(
         supabaseUrl = "https://rcgyceojtbimoqkbbvrx.supabase.co",
         supabaseKey = "sb_publishable_e1BrBK_VgxIsQF1-2sXHsQ_TvSSgFRP"
     ) {
-        install(Auth)
+        install(Auth) {
+            alwaysAutoRefresh = true
+            autoLoadFromStorage = true
+        }
+
         install(Postgrest)
     }
 }
